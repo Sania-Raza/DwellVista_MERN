9d7f01827a21d239d2c3fa90ac7824fabb4c7e52
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-17 17:20:33 +0500

    changes made

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
-import express from'express';
-const app= express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dns from "dns";
 
+dotenv.config();
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-app.listen(3000 ,()=>{
-    console.log('Server running at PORT 3000')
-})
-// module.exports = app;
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB error:", err));
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running at PORT 3000");
+});
