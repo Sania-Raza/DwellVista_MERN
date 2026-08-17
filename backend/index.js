5f058d4c8f20f41769fd7b6d20aaac00ace69752
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-17 22:59:33 +0500

    auth started

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import dns from "dns";
 import userRoute from "./routes/userRoute.js";
 dotenv.config();
-
+import authRoute from "./routes/authRoute.js";
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 mongoose
@@ -13,7 +13,11 @@ mongoose
   .catch((err) => console.log("MongoDB error:", err));
 
 const app = express();
+app.use(express.json());
+
+
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(3000, () => {
   console.log("Server running at PORT 3000");
