0d0d8f5e19daa5af22d2aa0d7fc4c6218a9c2e61
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-17 22:09:39 +0500

    added route

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "dns";
-
+import userRoute from "./routes/userRoute.js";
 dotenv.config();
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
@@ -13,6 +13,7 @@ mongoose
   .catch((err) => console.log("MongoDB error:", err));
 
 const app = express();
+app.use("/api/user", userRoute);
 
 app.listen(3000, () => {
   console.log("Server running at PORT 3000");
