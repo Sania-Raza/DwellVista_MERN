3a39a19c3106e53e2b7f2f5c885db6a1e13d2927
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-18 23:17:59 +0500

    added signin functionality

--- backend/index.js
+++ backend/index.js
@@ -14,19 +14,21 @@ mongoose
 
 const app = express();
 app.use(express.json());
-app.use((err,req,res,next)=>{
-  const statusCode = err.statusCode ||500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success:false,
-    statusCode,
-    message,
-  });
-});
+
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 app.listen(3000, () => {
   console.log("Server running at PORT 3000");
 });
