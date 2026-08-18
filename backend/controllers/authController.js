9f59a21c2c2c0322c4a2ede59b3a8bdfa2b99806
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-18 17:07:56 +0500

    added signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,6 +4,12 @@ import { errorHandler } from "../utlis/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
@@ -13,6 +19,13 @@ export const signup = async (req, res, next) => {
       message: "user created",
     });
   } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "this user already exists",
+      });
+    }
+
     next(error);
   }
 };
