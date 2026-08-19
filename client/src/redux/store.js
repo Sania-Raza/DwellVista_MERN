ff572e72a742c103e3bfb29293f742d88fd62cac
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-19 13:41:11 +0500

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,41 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
+import { persistReducer, persistStore } from "redux-persist";
+
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+
+  setItem: (key, value) => {
+    localStorage.setItem(key, value);
+    return Promise.resolve();
   },
+
+  removeItem: (key) => {
+    localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
