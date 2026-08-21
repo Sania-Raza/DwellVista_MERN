d6895bbe573a9e9299698beb2cda3772ef89c6aa
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-21 23:17:20 +0500

    added update user listing

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   createListing,
   deleteListing,
-//   updateListing,
+  updateListing,
 //   getListing,
 //   getListings,
 } from "../controllers/listingController.js";
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
-// router.post("/update/:id", verifyToken, updateListing);
+router.post("/update/:id", verifyToken, updateListing);
 // router.get("/get/:id", getListing);
 // router.get("/get", getListings);
 
