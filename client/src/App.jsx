a75b4ecbb9b048937376019b332efb4b6336d1d1
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-21 20:51:51 +0500

    complete listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from "./pages/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
