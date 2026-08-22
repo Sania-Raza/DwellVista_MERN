ba66de708f9ebb5051a6374abaa4baf20a2ffdc1
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-22 13:05:43 +0500

    create search api

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -4,7 +4,7 @@ import {
   deleteListing,
   updateListing,
    getListing,
-//   getListings,
+ getListings,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utlis/verifyUser.js";
 
@@ -14,6 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
-// router.get("/get", getListings);
+router.get("/get", getListings);
 
 export default router;
