8db5023e1431e856f95545c9910b24b6101648ea
Sania-Raza <sania.raza.pk@gmail.com>
2026-08-22 12:04:14 +0500

    complete listing display

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -90,9 +90,11 @@ export default function Listing() {
           )}
           <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4">
             <p className="text-2xl font-semibold">
-              {listing.name} - ${" "}
+              {listing.name} - PKR{" "}
               {listing.offer
-                ? listing.discountPrice.toLocaleString("en-US")
+                ? (listing.regularPrice - listing.discountPrice).toLocaleString(
+                    "en-US",
+                  )
                 : listing.regularPrice.toLocaleString("en-US")}
               {listing.type === "rent" && " / month"}
             </p>
@@ -101,12 +103,12 @@ export default function Listing() {
               {listing.address}
             </p>
             <div className="flex gap-4">
-              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+              <p className="bg-red-900 w-full max-w-50 text-white text-center p-1 rounded-md">
                 {listing.type === "rent" ? "For Rent" : "For Sale"}
               </p>
               {listing.offer && (
-                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                <p className="bg-green-900 w-full max-w-50 text-white text-center p-1 rounded-md">
+                  PKR {listing.discountPrice} OFF
                 </p>
               )}
             </div>
